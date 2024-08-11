7227f6d283aaf3e4acaf8693c94d8941f804e981
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-08-12 01:39:27 +0300

    feat: Minify HTML in login and registration routes & prepare static files

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
+from utilities.prepare import prepare_static_files
+from utilities.minimize import minify_html
 
+prepare_static_files()
 
 app = Flask(__name__)
 
 
 @app.route('/login')
-def login(): return render_template("pages/login.j2")
+def login(): return minify_html(render_template("pages/login.j2"))
 
 
 @ app.route('/register')
-def register(): return render_template("pages/register.j2")
+def register(): return minify_html(render_template("pages/register.j2"))
 
 
 if __name__ == '__main__':
