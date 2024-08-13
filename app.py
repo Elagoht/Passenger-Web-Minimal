08fcadfec9164b7bc64450175749f6ac5c13f34a
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-08-13 20:11:16 +0300

    chore: half of dashboard

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from utilities.prepare import prepare_static_files
 from utilities.minimize import minify_html
 from utilities.authorization import public_page, private_page
+from requests import get
 
 prepare_static_files()
 
@@ -28,7 +29,28 @@ def register():
 
 @app.route("/dashboard")
 @private_page
-def dashboard(): return minify_html(render_template("pages/dashboard.j2"))
+def dashboard():
+    headers = {
+        "Authorization": f"Bearer {request.cookies.get("accessToken", "")}"
+    }
+    data = get("http://localhost:3000/stats", headers=headers).json()
+
+    return minify_html(render_template(
+        "pages/dashboard.j2",
+        total_count=data["totalCount"],
+        average_length=data["averageLength"],
+        unique_platforms_count=data["uniquePlatformsCount"],
+        unique_passphrases=data["uniquePassphrases"],
+        most_accessed=data["mostAccessed"],
+        common_by_platform=data["commonByPlatform"],
+        percentage_of_common=data["percentageOfCommon"],
+        most_common=data["mostCommon"],
+        strengths=data["strengths"],
+        average_strength=data["averageStrength"],
+        weak_passphrases=data["weakPassphrases"],
+        medium_passphrases=data["mediumPassphrases"],
+        strong_passphrases=data["strongPassphrases"]
+    ))
 
 
 if __name__ == "__main__":
