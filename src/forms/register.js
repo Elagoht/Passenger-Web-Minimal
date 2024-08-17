24a0366f270b01c2fd613b86c044d57b33c40f52
Furkan Baytekin <furkanbaytekin@gmail.com>
2024-08-17 18:44:11 +0300

    feat: Add registration form and functionality

---
+++ src/forms/register.js
@@ -0,0 +1,27 @@
+import { postRegister } from "../services/authServices.js"
+
+const registerFormAction = async (event) => {
+  event.preventDefault()
+
+  const form = new FormData(event.target)
+
+  const username = form.get("username")
+  const passphrase = form.get("passphrase")
+
+  if (typeof username !== "string" || typeof passphrase !== "string")
+    return alert("Invalid input")
+
+  const response = await postRegister({
+    username: username,
+    passphrase: passphrase
+  })
+
+  const data = await response.json()
+
+  if (!response.ok)
+    return document.getElementById("error-message").innerText = data.message
+
+  window.location.href = "/dashboard"
+}
+
+window.registerFormAction = registerFormAction
